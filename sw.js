14370bdc6c6107c2fee0d7facab4fc6c8c97b354
Shailesh Vasandani <github@shaile.sh>
2023-05-15 10:01:39 -0400

    Avoid caching if request should be live

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
 self.addEventListener("install", () => {});
 self.addEventListener("fetch", (event) => {
-  event.respondWith((async () => {
-    try {
-      const res = await fetch(event.request);
-      const cache = await caches.open('cache');
-      cache.put(event.request.url, res.clone());
-      return res;
-    } catch {
-      return caches.match(event.request);
-    }
-  })());
+  event.respondWith(
+    (async () => {
+      const url = new URL(event.request.url);
+      if (url.host.includes("freebee.fun")) return fetch(event.request);
+
+      try {
+        const res = await fetch(event.request);
+        const cache = await caches.open("cache");
+        cache.put(event.request.url, res.clone());
+        return res;
+      } catch {
+        return caches.match(event.request);
+      }
+    })()
+  );
 });
