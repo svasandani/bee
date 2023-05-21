644fd5c8feff237bbbdcb92bd58bb7c887f554fe
Shailesh Vasandani <github@shaile.sh>
2023-05-20 23:19:02 -0400

    Modify service worker strategy

--- sw.js
+++ sw.js
@@ -1,18 +1,52 @@
-self.addEventListener("install", () => {});
+const CACHE_NAME = "bee_Site";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      await cache.addAll(["/", "/dict.txt", "/site.webmanifest"]);
+    })()
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cacheNames = await caches.keys();
+
+      await Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })()
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
-      const url = new URL(event.request.url);
-      if (url.host.includes("freebee.fun")) return fetch(event.request);
+      const cache = await caches.open(CACHE_NAME);
 
-      try {
-        const res = await fetch(event.request);
-        const cache = await caches.open("cache");
-        cache.put(event.request.url, res.clone());
-        return res;
-      } catch {
-        return caches.match(event.request);
-      }
+      const cachedResponsePromise = await cache.match(event.request);
+      const networkResponsePromise = fetch(event.request);
+
+      // cache these in the background
+      (async () => {
+        try {
+          const networkResponse = await networkResponsePromise;
+          if (networkResponse.ok) {
+            await cache.put(event.request, networkResponse.clone());
+          }
+        } catch {
+          console.log("Couldn't revalidate a request, maybe we're offline?");
+        }
+      })();
+
+      return cachedResponsePromise || networkResponsePromise;
     })()
   );
 });
